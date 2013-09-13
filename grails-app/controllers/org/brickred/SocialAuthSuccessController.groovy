d62a6b80e3f1b9b3acf7af2d1c97dce094781693
Okal Otieno <okal@justokal.com>
2013-09-13 11:41:33 +0300

    Add tutorial code to socialAuthSuccess controller

--- grails-app/controllers/org/brickred/SocialAuthSuccessController.groovy
+++ grails-app/controllers/org/brickred/SocialAuthSuccessController.groovy
@@ -1,6 +1,18 @@
 package org.brickred
 
+import org.brickred.socialauth.Contact
+import org.brickred.socialauth.Profile
+
+import de.deltatree.social.web.filter.api.SASFHelper
+import de.deltatree.social.web.filter.api.SASFStaticHelper
+
 class SocialAuthSuccessController {
 
-    def index() { }
+    def index = { 
+		SASFHelper helper = SASFStaticHelper.getHelper(request)
+		Profile profile = helper.getProfile()
+		def contactsList = helper.getContactList()
+		println profile
+		[profile:profile, contacts:contactsList]
+	}
 }
