f4adf74005997c7a170c670f15551bf6f195b904
Okal Otieno <okal@justokal.com>
2013-09-13 11:38:11 +0300

    Add tutorial code to SocialAuthController

--- grails-app/controllers/org/brickred/SocialAuthController.groovy
+++ grails-app/controllers/org/brickred/SocialAuthController.groovy
@@ -1,6 +1,33 @@
 package org.brickred
 
+import org.brickred.socialauth.SocialAuthManager
+import org.codehaus.groovy.grails.web.pages.ext.jsp.GroovyPagesPageContext
+
+import de.deltatree.social.web.filter.api.SASFStaticHelper
+import de.deltatree.social.web.filter.api.SASFHelper
+
+
 class SocialAuthController {
 
-    def index() { }
+    def index = {
+		println "id::: ${params.id}"
+		redirect uri: "/SAF/SocialAuth?id=${params.id}"
+	}
+
+	def signout = {
+		SASFHelper helper = SASFStaticHelper.getHelper(request)
+		SocialAuthManager socialAuthManager
+
+		if(helper != null) {
+			socialAuthManager = helper.getAuthManager()
+			if (socialAuthManager != null) {
+				socialAuthManager.disconnectProvider(params.id)
+			} else {
+				flash.message = "Unable to logout from ${params.id}"
+			}
+		} else {
+			flash.message = "Unable to logout from ${params.id}"
+		}
+		redirect uri: "/index.gsp"
+	}
 }
